f3cbae06674149e25374f6d03c852f4e90c4cfe8
colombefioren <colomberakotonjanahary@gmail.com>
2025-12-17 12:05:46 +0300

    feat: implement find dish by id method and a test in main class

--- src/main/java/com/jdbctd2/Main.java
+++ src/main/java/com/jdbctd2/Main.java
@@ -1,7 +1,16 @@
 package com.jdbctd2;
 
+import com.jdbctd2.model.Dish;
+import com.jdbctd2.model.Ingredient;
+import com.jdbctd2.repository.DataRetriever;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    DataRetriever dataRetriever = new DataRetriever();
+
+    // a) Dish findDishById(Integer id)
+    System.out.println("===> Dish findDishById(Integer id) <===");
+    Dish dish = dataRetriever.findDishById(1);
+    System.out.println("Dish with ID = 1 : " + dish);
   }
 }
