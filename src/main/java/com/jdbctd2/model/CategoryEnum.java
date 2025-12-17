36f89b05346e33f5cad5d56ac482139ea4724e8e
colombefioren <colomberakotonjanahary@gmail.com>
2025-12-17 11:03:05 +0300

    feat: create ingredient and dish class models

---
+++ src/main/java/com/jdbctd2/model/CategoryEnum.java
@@ -0,0 +1,5 @@
+package com.jdbctd2.model;
+
+public enum CategoryEnum {
+    VEGETABLE, ANIMAL, MARINE, DAIRY, OTHER
+}
---
+++ src/main/java/com/jdbctd2/model/Dish.java
@@ -0,0 +1,75 @@
+package com.jdbctd2.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Dish {
+    private int id;
+    private String name;
+    private DishTypeEnum dishType;
+    private List<Ingredient> ingredients;
+
+    public Dish() {
+    }
+
+    public Dish(int id, String name, DishTypeEnum dishType, List<Ingredient> ingredients) {
+        this.id = id;
+        this.name = name;
+        this.dishType = dishType;
+        this.ingredients = ingredients;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DishTypeEnum getDishType() {
+        return dishType;
+    }
+
+    public void setDishType(DishTypeEnum dishType) {
+        this.dishType = dishType;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dish dish = (Dish) o;
+        return id == dish.id && Objects.equals(name, dish.name) && dishType == dish.dishType && Objects.equals(ingredients, dish.ingredients);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, dishType, ingredients);
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dishType=" + dishType +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+}
