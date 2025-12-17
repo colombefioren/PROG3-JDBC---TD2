e80c988449fc631441b1d0ec7d1546dbd95db6e8
colombefioren <colomberakotonjanahary@gmail.com>
2025-12-17 11:18:26 +0300

    feat: create interfaces containing methods that needs to be implemented

---
+++ src/main/java/com/jdbctd2/repository/DataRetriever.java
@@ -0,0 +1,3 @@
+package com.jdbctd2.repository;
+
+public class DataRetriever {}
---
+++ src/main/java/com/jdbctd2/repository/IngredientRepository.java
@@ -0,0 +1,15 @@
+package com.jdbctd2.repository;
+
+import com.jdbctd2.model.CategoryEnum;
+import com.jdbctd2.model.Ingredient;
+
+import java.util.List;
+
+public interface IngredientRepository {
+
+    List<Ingredient> findIngredients(int page, int size);
+
+    List<Ingredient> createIngredients(List<Ingredient> newIngredients);
+
+    List<Ingredient> findIngredientsByCriteria(String ingredientNmae, CategoryEnum category, String dishName, int page, int size);
+}
