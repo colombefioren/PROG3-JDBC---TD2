fd508d2b112cdde721093a1226659c9cf67a04b0
colombefioren <colomberakotonjanahary@gmail.com>
2025-12-17 11:08:12 +0300

    feat: implement DBConnection class which grants connexion between my app and my local db

---
+++ src/main/java/com/jdbctd2/db/DBConnection.java
@@ -0,0 +1,15 @@
+package com.jdbctd2.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private final String JDBC_URL = System.getenv("JDBC_URL");
+    private final String USERNAME = System.getenv("USERNAME");
+    private final String PASSWORD = System.getenv("PASSWORD");
+
+    public Connection getDBConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+}
