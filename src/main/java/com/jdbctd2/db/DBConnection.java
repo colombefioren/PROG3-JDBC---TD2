415ec8cb4359ae97c731b52f493b30ead2ca82fa
colombefioren <colomberakotonjanahary@gmail.com>
2025-12-17 12:30:09 +0300

    refactor: user system env

--- src/main/java/com/jdbctd2/db/DBConnection.java
+++ src/main/java/com/jdbctd2/db/DBConnection.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private final String JDBC_URL = "jdbc:postgresql://localhost:5433/mini_dish_db";
-    private final String USERNAME = "mini_dish_db_manager";
-    private final String PASSWORD = "123456";
+  private final String JDBC_URL = System.getenv("JDBC_URL");
+  private final String USERNAME = System.getenv("USERNAME");
+  private final String PASSWORD = System.getenv("PASSWORD");
 
-    public Connection getDBConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-    }
+  public Connection getDBConnection() throws SQLException {
+    return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+  }
 }
--- src/main/java/com/jdbctd2/repository/DataRetriever.java
+++ src/main/java/com/jdbctd2/repository/DataRetriever.java
@@ -5,7 +5,6 @@ import com.jdbctd2.model.CategoryEnum;
 import com.jdbctd2.model.Dish;
 import com.jdbctd2.model.DishTypeEnum;
 import com.jdbctd2.model.Ingredient;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,95 +12,99 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataRetriever implements IngredientRepository, DishRepository{
+public class DataRetriever implements IngredientRepository, DishRepository {
 
-    private final DBConnection dbConnection;
+  private final DBConnection dbConnection;
 
-    public DataRetriever() {
-        this.dbConnection = new DBConnection();
-    }
+  public DataRetriever() {
+    this.dbConnection = new DBConnection();
+  }
 
-    @Override
-    public Dish findDishById(Integer id) {
-        String sql = """
+  @Override
+  public Dish findDishById(Integer id) {
+    String sql =
+        """
                     select ingredient.id, ingredient.price, ingredient.category,ingredient.name,
                    dish.id as dish_id, dish.name as dish_name, dish.dish_type as dish_type
-                    from ingredient right join dish on dish.id = ingredient.id_dish
+                    from ingredient left join dish on dish.id = ingredient.id_dish
                     where id_dish = ?;
                    """;
 
-        try(Connection con = dbConnection.getDBConnection();
-            PreparedStatement ps = con.prepareStatement(sql)){
-                ps.setInt(1,id);
-                ResultSet rs = ps.executeQuery();
-                return createDishFromResultSet(rs);
+    try (Connection con = dbConnection.getDBConnection();
+        PreparedStatement ps = con.prepareStatement(sql)) {
+      ps.setInt(1, id);
+      ResultSet rs = ps.executeQuery();
+      return createDishFromResultSet(rs);
 
-        }catch (SQLException e){
-            throw new RuntimeException(e);
-        }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Dish saveDish(Dish dishToSave) {
+    return null;
+  }
+
+  @Override
+  public List<Dish> findDishesByIngredientName(String IngredientName) {
+    return List.of();
+  }
+
+  @Override
+  public List<Ingredient> findIngredients(int page, int size) {
+    String sql =
+"""
+        select ingredient.id, ingredient.name, ingredient.price, ingredient.category, id_dish, dish.name, dish.dish_type
+        from ingredient
+        left join dish ON dish.id = ingredient.id_dish
+        order by ingredient.id
+        limit ? offset ?
+""";
+    return List.of();
+  }
+
+  @Override
+  public List<Ingredient> createIngredients(List<Ingredient> newIngredients) {
+    return List.of();
+  }
+
+  @Override
+  public List<Ingredient> findIngredientsByCriteria(
+      String ingredientNmae, CategoryEnum category, String dishName, int page, int size) {
+    return List.of();
+  }
+
+  private Dish createDishFromResultSet(ResultSet rs) throws SQLException {
+    Dish dish = null;
+    List<Ingredient> ingredients = new ArrayList<>();
+
+    while (rs.next()) {
+      if (dish == null) {
+        dish = new Dish();
+        dish.setId(rs.getInt("dish_id"));
+        dish.setName(rs.getString("dish_name"));
+        dish.setDishType(DishTypeEnum.valueOf(rs.getString("dish_type")));
+      }
+      ingredients.add(createIngredientFromResultSet(rs, dish));
     }
 
-    @Override
-    public Dish saveDish(Dish dishToSave) {
-        return null;
+    if (dish != null) {
+      dish.setIngredients(ingredients);
     }
 
-    @Override
-    public List<Dish> findDishesByIngredientName(String IngredientName) {
-        return List.of();
-    }
+    return dish;
+  }
 
-    @Override
-    public List<Ingredient> findIngredients(int page, int size) {
-        String
-        return List.of();
-    }
+  private Ingredient createIngredientFromResultSet(ResultSet rs, Dish dish) throws SQLException {
+    Ingredient ing = new Ingredient();
+    ing.setId(rs.getInt("id"));
+    ing.setName(rs.getString("name"));
+    ing.setPrice(rs.getDouble("price"));
+    ing.setCategory(CategoryEnum.valueOf(rs.getString("category")));
 
-    @Override
-    public List<Ingredient> createIngredients(List<Ingredient> newIngredients) {
-        return List.of();
-    }
+    ing.setDish(dish);
 
-    @Override
-    public List<Ingredient> findIngredientsByCriteria(String ingredientNmae, CategoryEnum category, String dishName, int page, int size) {
-        return List.of();
-    }
-
-    private Dish createDishFromResultSet(ResultSet rs) throws SQLException {
-        Dish dish = null;
-        List<Ingredient> ingredients = new ArrayList<>();
-
-        while (rs.next()) {
-            if (dish == null) {
-                dish = new Dish();
-                dish.setId(rs.getInt("dish_id"));
-                dish.setName(rs.getString("dish_name"));
-                dish.setDishType(
-                        DishTypeEnum.valueOf(rs.getString("dish_type"))
-                );
-            }
-            ingredients.add(createIngredientFromResultSet(rs, dish));
-        }
-
-        if (dish != null) {
-            dish.setIngredients(ingredients);
-        }
-
-        return dish;
-
-    }
-
-    private Ingredient createIngredientFromResultSet(ResultSet rs, Dish dish) throws SQLException {
-        Ingredient ing = new Ingredient();
-        ing.setId(rs.getInt("id"));
-        ing.setName(rs.getString("name"));
-        ing.setPrice(rs.getDouble("price"));
-        ing.setCategory(
-                CategoryEnum.valueOf(rs.getString("category"))
-        );
-
-       ing.setDish(dish);
-
-        return ing;
-    }
+    return ing;
+  }
 }
