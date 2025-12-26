f348f9df737e14d2c41e46644d82f0fdf15c159f
colombefioren <colomberakotonjanahary@gmail.com>
2025-12-26 10:28:21 +0300

    feat: add new test cases for method findIngredients and findDishById

--- src/main/java/com/jdbctd2/Main.java
+++ src/main/java/com/jdbctd2/Main.java
@@ -9,14 +9,30 @@ public class Main {
   public static void main(String[] args) {
     DataRetriever dataRetriever = new DataRetriever();
 
-    // a) Dish findDishById(Integer id)
-    System.out.println("===> Dish findDishById(Integer id) <===");
-    Dish dish = dataRetriever.findDishById(1);
-    System.out.println("id=1 : " + dish);
+    // a) Dish findDishById(Integer id) - id = 1
+    System.out.println("===> Dish findDishById(Integer id) | id = 1 <===");
+    Dish dishA = dataRetriever.findDishById(1);
+    System.out.println("id=1 : " + dishA);
 
-    // b) List<Ingredient> findIngredients(int page, int size)
-    System.out.println("\n===> List<Ingredient> findIngredients(int page, int size) <===");
-    List<Ingredient> ingredientList = dataRetriever.findIngredients(2, 2);
-    System.out.println("page=2,size=2 : " + ingredientList);
+    // b) Dish findDishById(Integer id) - id = 1
+    System.out.println("===> Dish findDishById(Integer id) | id = 999 <===");
+    try {
+      Dish dishB = dataRetriever.findDishById(999);
+      System.out.println("id=1 : " + dishB);
+    } catch (RuntimeException e) {
+      System.out.println("Dish with id 999 not found. Error: " + e);
+    }
+
+    // c) List<Ingredient> findIngredients(int page, int size) - page=2 size=2
+    System.out.println(
+        "\n===> List<Ingredient> findIngredients(int page, int size) | page=2,size=2 <===");
+    List<Ingredient> ingredientListC = dataRetriever.findIngredients(2, 2);
+    System.out.println("page=2,size=2 : " + ingredientListC);
+
+    // d) List<Ingredient> findIngredients(int page, int size) - page=3 size=5
+    System.out.println(
+        "\n===> List<Ingredient> findIngredients(int page, int size) | page=3,size=5 <===");
+    List<Ingredient> ingredientListD = dataRetriever.findIngredients(3, 5);
+    System.out.println("page=2,size=2 : " + ingredientListD);
   }
 }
