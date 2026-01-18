9ed0369c5fbdb96a2c5fb2a5582e4f428ef4c1f6
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-18 16:12:04 +0300

    feat: add new_schema sql file which contains script that normalize db

---
+++ sql/new_schema.sql
@@ -0,0 +1,34 @@
+create type unit_type as enum ('KG', 'L', 'PCS');
+
+-- create a dish_ingredient table
+create table DishIngredient
+(
+    id                serial primary key,
+    id_dish           int            not null,
+    id_ingredient     int            not null,
+    quantity_required numeric(10, 3) not null,
+    unit              unit_type      not null,
+
+    constraint fk_dish
+        foreign key (id_dish)
+            references Dish (id)
+            on delete cascade,
+    constraint fk_ingredient
+        foreign key (id_ingredient)
+            references Ingredient (id)
+            on delete cascade,
+    constraint unique_dish_ingredient
+        unique (id_dish, id_ingredient)
+);
+
+-- rename price to selling price if exists
+ALTER TABLE Dish
+    RENAME COLUMN price TO selling_price;
+
+-- add selling_price if it doesnt
+ALTER TABLE Dish
+    ADD COLUMN IF NOT EXISTS selling_price NUMERIC;
+
+-- delete id_dish column in ingredient since it's not necessary anymore
+ALTER TABLE ingredient
+    DROP COLUMN IF EXISTS id_dish;
