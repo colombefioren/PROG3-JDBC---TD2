69548ce924597230471bd3b66c5e6dfe3e09ea90
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-21 11:11:56 +0300

    feat: write stock_schema sql which contains scripts that add new stock_movement table

---
+++ sql/stock_schema.sql
@@ -0,0 +1,12 @@
+create table stock_moment
+(
+    id            serial primary key,
+    id_ingredient int                                 not null,
+    quantity      int                                 not null,
+    unit          unit_type                           not null,
+    datetime      timestamp default current_timestamp not null,
+    type          movement_type                       not null,
+    constraint fk_id_ing foreign key (id_ingredient)
+        references Ingredient (id)
+        on delete cascade
+);
---
+++ src/main/java/com/jdbctd2/model/StockMoment.java
@@ -0,0 +1,3 @@
+package com.jdbctd2.model;
+
+public class StockMoment {}
