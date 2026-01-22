7ee453a36b28501ab9321651a7616424c03ff0f4
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-22 06:49:58 +0300

    fix: rewrite initializeDB method to consider the new stock movement table

--- src/main/java/com/jdbctd2/repository/DataRetriever.java
+++ src/main/java/com/jdbctd2/repository/DataRetriever.java
@@ -21,7 +21,7 @@ public class DataRetriever implements IngredientRepository, DishRepository {
   public void initializeDB() {
     String clearTablesSql =
         """
-                    truncate table ingredient, dish, dish_ingredient restart identity cascade
+                    truncate table ingredient, dish, dish_ingredient, stock_movement restart identity cascade
                     """;
 
     String insertDishSql =
@@ -54,6 +54,27 @@ public class DataRetriever implements IngredientRepository, DishRepository {
                            (4, 5, 0.20, 'KG')
                     """;
 
+    String insertStockMovementSql =
+"""
+insert into stock_movement (id_ingredient, quantity, unit, creation_datetime, type)
+values (1, 5.0, 'KG', '2024-01-05 08:00', 'IN'),
+       (1, 0.2, 'KG', '2024-01-06 12:00', 'OUT'),
+       (2, 4.0, 'KG', '2024-01-05 08:00', 'IN'),
+       (2, 0.15, 'KG', '2024-01-06 12:00', 'OUT'),
+       (3, 10.0, 'KG', '2024-01-04 09:00', 'IN'),
+       (3, 1.0, 'KG', '2024-01-06 13:00', 'OUT'),
+       (4, 3.0, 'KG', '2024-01-05 10:00', 'IN'),
+       (4, 0.3, 'KG', '2024-01-06 14:00', 'OUT'),
+       (5, 2.5, 'KG', '2024-01-05 10:00', 'IN'),
+       (5, 0.2, 'KG', '2024-01-06 14:00', 'OUT')
+""";
+
+    String stockMovementSequenceSql =
+"""
+select setval('stock_movement_id_seq', (select max(id) from stock_movement));
+
+""";
+
     String dishSequenceSql =
         """
                     select setval('dish_id_seq', (select max(id) from dish));
@@ -79,6 +100,8 @@ public class DataRetriever implements IngredientRepository, DishRepository {
       stmt.executeUpdate(insertDishSql);
       stmt.executeUpdate(insertIngredientSql);
       stmt.executeUpdate(insertDishIngSql);
+      stmt.executeUpdate(insertStockMovementSql);
+      stmt.executeQuery(stockMovementSequenceSql);
       stmt.executeQuery(dishSequenceSql);
       stmt.executeQuery(ingSequenceSql);
       stmt.executeQuery(dishIngSequenceSql);
