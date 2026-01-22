e2f1ecc102485cdfdd7751225d5d935c1ffd4530
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-22 05:35:30 +0300

    refactor: rename datetime column in stock_movement to creation_datetime

--- sql/stock_schema.sql
+++ sql/stock_schema.sql
@@ -2,12 +2,12 @@ create type movement_type as enum ('IN','OUT');
 
 create table stock_movement
 (
-    id            serial primary key,
-    id_ingredient int                                 not null,
-    quantity      int                                 not null,
-    unit          unit_type                           not null,
-    datetime      timestamp default current_timestamp not null,
-    type          movement_type                       not null,
+    id                serial primary key,
+    id_ingredient     int                                 not null,
+    quantity          int                                 not null,
+    unit              unit_type                           not null,
+    creation_datetime timestamp default current_timestamp not null,
+    type              movement_type                       not null,
     constraint fk_id_ing foreign key (id_ingredient)
         references Ingredient (id)
         on delete cascade
