80de7d5ac74c7286d2eb93b0ebf7e19459d0fb28
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-25 16:01:00 +0300

    feat: write schema sql file which contains script that create needed tables

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+create type category as enum ('VEGETABLE','ANIMAL','MARINE','DAIRY','OTHER');
+create type dish_type as enum ('START','MAIN','DESSERT');
+
+create table Dish (
+  id serial primary key,
+  name varchar(255) not null,
+  dish_type dish_type not null
+);
+
+create table Ingredient (
+    id serial primary key,
+    name varchar(255) not null,
+    price numeric(10,2) not null,
+    category category not null,
+    id_dish int,
+    constraint dish_fk
+        foreign key (id_dish)
+        references Dish(id)
+);
