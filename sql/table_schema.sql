51eb022134806fc8f81b19f1cbe09e6ce9ac144c
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-29 10:19:52 +0300

    feat: write table schema sql which contains script that create new table table

---
+++ sql/table_schema.sql
@@ -0,0 +1,18 @@
+create table "table"
+(
+    id     serial primary key,
+    number int not null unique
+);
+
+alter table "order"
+    add column if not exists id_table              int,
+    add column if not exists installation_datetime timestamp default current_timestamp,
+    add column if not exists departure_datetime    timestamp,
+    add constraint table_fk foreign key (id_table) references "table" (id) on delete set null;
+
+insert into "table" (id, number)
+values (1, 1),
+       (2, 2),
+       (3, 3);
+
+select setval('table_id_seq', (select max(id) from "table"));
