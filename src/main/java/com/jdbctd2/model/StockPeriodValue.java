4eea602d03173dd0b7a3add1668c1405635b045b
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-18 06:09:32 +0300

    feat: specify method in stock movement interface

---
+++ src/main/java/com/jdbctd2/model/StockPeriodValue.java
@@ -0,0 +1,61 @@
+package com.jdbctd2.model;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class StockPeriodValue {
+  private int ingredientId;
+  private Instant period;
+  private double stockValue;
+
+  public StockPeriodValue(int ingredientId, Instant period, double stockValue) {
+    this.ingredientId = ingredientId;
+    this.period = period;
+    this.stockValue = stockValue;
+  }
+
+  public int getIngredientId() {
+    return ingredientId;
+  }
+
+  public Instant getPeriod() {
+    return period;
+  }
+
+  public double getStockValue() {
+    return stockValue;
+  }
+
+    public void setIngredientId(int ingredientId) {
+        this.ingredientId = ingredientId;
+    }
+
+    public void setPeriod(Instant period) {
+        this.period = period;
+    }
+
+    public void setStockValue(double stockValue) {
+        this.stockValue = stockValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        StockPeriodValue that = (StockPeriodValue) o;
+        return ingredientId == that.ingredientId && Double.compare(stockValue, that.stockValue) == 0 && Objects.equals(period, that.period);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredientId, period, stockValue);
+    }
+
+    @Override
+    public String toString() {
+        return "StockPeriodValue{" +
+                "ingredientId=" + ingredientId +
+                ", period=" + period +
+                ", stockValue=" + stockValue +
+                '}';
+    }
+}
--- src/main/java/com/jdbctd2/repository/interf/StockMovementRepository.java
+++ src/main/java/com/jdbctd2/repository/interf/StockMovementRepository.java
@@ -1,6 +1,7 @@
 package com.jdbctd2.repository.interf;
 
 import com.jdbctd2.model.StockMovement;
+import com.jdbctd2.model.StockPeriodValue;
 import com.jdbctd2.model.StockValue;
 import java.time.Instant;
 import java.util.List;
@@ -11,4 +12,6 @@ public interface StockMovementRepository {
   List<StockMovement> findStockMovementsByIngredientId(Integer ingredientId);
 
   StockValue getStockValueAt(Instant t, Integer ingredientIdentifier);
+
+  List<StockPeriodValue> getStockValues(String peiodicity, Instant intervalleMin, Instant intervalleMax);
 }
